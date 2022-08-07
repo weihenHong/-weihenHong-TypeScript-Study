483404fff4750ad94f24cdfcc11d523c3b88ac9d
ws <286572050@qq.com>
2022-08-08 00:02:57 +0800

    'day3'

--- app.ts
+++ app.ts
@@ -9,6 +9,17 @@ interface objA{
 interface objA{
     age: number
 }
-let obj:objA = {
-    name: '1'
+// let obj:objA = {
+    // name: '1'
+// }
+
+
+let arr:number[] = [1]
+
+// 前几次都是函数定义，最后一次是函数实现。
+function fn(params:number,params2:string):void
+function fn(params:string,params2:number):void
+function fn(params:string|number,params2:number | string):void{
 }
+
+fn(2,'')
